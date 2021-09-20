abc1844a6b96ec5773c1546e7a4c1cdb01c87027
ttcng <68971902+ttcng@users.noreply.github.com>
2021-09-20 21:48:59 +0100

    changing function order

--- recipe2.js
+++ recipe2.js
@@ -1,20 +1,6 @@
-function myFunction() {
-    // Get the checkbox
 
-    var checkBox = document.getElementByName("step6");
-    var length = checkbox.length
-    // Get the output text
-    var text = document.getElementById("text");
 
-    // If the checkbox is checked, display the output text
-    if (checkBox.checked == true){
-        allsteps();
-      } else {
-        alert("none");
-      }
-    }
-
-    function allsteps() {
+function allsteps() {
         var boxes = document.getElementById(checkbox)
         if (boxes.length === 6) {
             alert("Congratulations! Enjoy your meal!")
@@ -23,3 +9,19 @@ function myFunction() {
             alert("Have you completed all the steps?")
         }
     }
+
+function myFunction() {
+        // Get the checkbox
+    
+        var checkBox = document.getElementByName("step6");
+        var length = checkbox.length
+        // Get the output text
+        var text = document.getElementById("text");
+    
+        // If the checkbox is checked, display the output text
+        if (checkBox.checked == true){
+            allsteps();
+          } else {
+            alert("none");
+          }
+        }
