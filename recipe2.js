a9320d4eb3aa11377800807f87694ce9fa140c72
ttcng <68971902+ttcng@users.noreply.github.com>
2021-09-20 21:13:10 +0100

    attempting js checkbox

---
+++ recipe2.js
@@ -0,0 +1,28 @@
+document.getElementByTagName('step6').onclick = function() {
+    // access properties using this keyword
+    if ( this.checked ) {
+        // Returns true if checked
+        calculateCheckbox();
+    } else {
+        // Returns false if not checked
+    }
+};
+
+function calculateCheckbox() {
+    // get beauty products checkboxes contianer's reference
+    var el = document.getElementById('checkbox');
+  
+    // get beauty product input element reference in beauty products container
+    var products = el.getElementsByTagName('input');
+  
+    // get products length
+    var len = products.length;
+  
+    // call updateCost() function to onclick event on every checkbox
+    if (len===6) {
+        alert("Congratulations! Enjoy your meal")
+    }
+
+    
+  }
+
