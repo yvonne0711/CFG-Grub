1925ced7fc43b70ae5e4ee966f4de46a9461e104
yvonne <yvonnewong1107@gmail.com>
2021-09-22 19:07:55 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 function quiz(){
     i = 0
-    aler("Welcome to the quiz! Please type your answers as a,b or c.")
+    alert("Welcome to the quiz! Please type your answers as a,b or c.")
     var ans1 = prompt("What pasta do you feel like? a: Wheat b: Zucchini c: Wholewheat");
     var ans2 = prompt("What sauce would you be? A: Tomato B: Bechamel sauce C: Cheese sauce");
     var ans3 = prompt("How about meat?? A: Beef B: Lamb C: Quorn");
@@ -26,16 +26,16 @@ function quiz(){
 
     for (var i = 0; i < mostLetters.length; i++){
         if (temp === numofA) {
-            prompt("This week you are going to find a new hobby!")
-            prompt("Thank you for visiting!")
+            alert("This week you are going to find a new hobby!")
+            alert("Thank you for visiting!")
             return
         } else if (temp === numofB) {
-            prompt("This week you are going to make a lot of money!")
-            prompt("Thank you for visiting!")
+            alert("This week you are going to make a lot of money!")
+            alert("Thank you for visiting!")
             return
         } else if (temp === numofC) {
-            prompt("This week you are going to be happy and meet lots of new friends!")
-            prompt("Thank you for visiting!")
+            alert("This week you are going to be happy and meet lots of new friends!")
+            alert("Thank you for visiting!")
             return
         }
 
